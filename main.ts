07fbdf267b8c5925c91a6beebaa0fd95414ddd71
olavho <99804412+olavho@users.noreply.github.com>
2022-02-19 19:00:18 +0100

    changed to block mode

--- main.ts
+++ main.ts
@@ -13,43 +13,38 @@ function readNumberInRange (max: number, initialValue: number, label: string) {
     }
     return result
 }
-
 function setDate () {
     year = readNumberInRange(2025, year, "year: ")
     month = readNumberInRange(12, month, "mth: ")
     day = readNumberInRange(31, day, "day: ")
     timeanddate.setDate(month, day, year)
 }
-
+input.onButtonPressed(Button.AB, function () {
+    mode = 1
+    basic.clearScreen()
+    setDate()
+    setTime()
+    basic.showString(timeanddate.dateTime())
+    basic.clearScreen()
+    mode = 0
+})
 function setTime () {
     hour = readNumberInRange(24, hour, "hr: ")
     min = readNumberInRange(59, min, "min: ")
     sec = readNumberInRange(59, sec, "sec: ")
     timeanddate.set24HourTime(hour, min, sec)
 }
-
 function plotNumber (yLoc: number, value: number) {
     for (let index = 0; index <= 4; index++) {
-        if ((Math.idiv(value, 2 ** index) % 2) == 1) {
+        if (Math.idiv(value, 2 ** index) % 2 == 1) {
             led.plot(4 - index, yLoc)
         } else {
             led.unplot(4 - index, yLoc)
         }
     }
 }
-
-input.onButtonPressed(Button.AB, function () {
-    mode = 1;
-    basic.clearScreen()
-    setDate()
-    setTime()
-    basic.showString(timeanddate.dateTime());
-    basic.clearScreen();
-    mode = 0;
-});
-
-
 let timeStr = ""
+let mode = 0
 let result = 0
 let sec = 0
 let year = 0
@@ -57,7 +52,7 @@ let month = 0
 let day = 0
 let hour = 0
 let min = 0
-let mode = 0 // 0: clock, 1: date / temperature / light, 2: edit clock, 3: edit date
+// 0: clock, 1: date / temperature / light, 2: edit clock, 3: edit date
 min = 50
 hour = 18
 day = 18
@@ -71,24 +66,19 @@ min = 44
 sec = 0
 timeanddate.setDate(month, day, year)
 timeanddate.set24HourTime(hour, min, sec)
-
 basic.forever(function () {
     if (mode == 0) {
         // YYYY-MM-DD HH:MM.SS
         serial.writeLine(timeanddate.dateTime())
         timeStr = timeanddate.dateTime()
-
         hour = parseInt(timeStr.substr(11, 2))
-        plotNumber(0, hour);
-
+        plotNumber(0, hour)
         min = parseInt(timeStr.substr(14, 2))
-        plotNumber(1, Math.idiv(min, 32));
+        plotNumber(1, Math.idiv(min, 32))
         plotNumber(2, min)
-
         sec = parseInt(timeStr.substr(17, 2))
-        plotNumber(3, Math.idiv(sec, 32));
+        plotNumber(3, Math.idiv(sec, 32))
         plotNumber(4, sec)
     }
-    
     pause(200);
 })
